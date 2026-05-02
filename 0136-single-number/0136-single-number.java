154fcafd6f40927328c8dc153c298606c112957c
TARUN KUMAR AGNIHOTRI <tarunagnihotri534@gmail.com>
2026-05-02 15:02:20 +0530

    Time: 1 ms (99.97%), Space: 46.7 MB (91.47%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-        
+
         for (int num : nums) {
-            result ^= num;  // XOR operation
+            result ^= num;  // XOR cancels duplicates
         }
-        
+
         return result;
     }
 }
